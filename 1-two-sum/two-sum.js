ec80247d625a0cfab7d23a6c2dc90308e5eb8942
codeX4r <117584477+codeX4r@users.noreply.github.com>
2026-08-25 04:32:42 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (47.29%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,12 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0 ; i<nums.length-1; i++){
-        for(let j =  i+1 ; j<nums.length ; j++ ){
-            if(nums[i]+nums[j]==target){
-                return [i,j]
-            }
-        }
-    }
-    return []
+   let i=0
+   const map = new Map()
+   while(i<nums.length){
+    let needed = target - nums[i]
+    if(map.has(needed))return [map.get(needed),i]
+    map.set(nums[i],i)
+    i++
+   }
+return []
 }
